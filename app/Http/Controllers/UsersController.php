8c10206b5e3f3d40edfa7518e079b20fe26dd13d
lizhufeng <lzhf09@126.com>
2017-08-28 21:21:47 +0800

    update prj

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //
+    
     public function index()
     {
         $users = [
@@ -16,11 +16,25 @@ class UsersController extends Controller
             ],
             '1' => [
                 'first_name' => 'Jessica',
-                'last_name' => 'Al'
+                'last_name' => 'Alba'
             ]
         ];
-        // return $users;
+
         // 这行变了，表示 $users 这个变量会传递到view中
         return view('admin.users.index', compact('users'));
     }
+    // public function index()
+    // {
+    //     $users = [
+    //         '0' => [
+    //             'first_name' => 'Renato',
+    //             'last_name' => 'Hysa'
+    //         ],
+    //         '1' => [
+    //             'first_name' => 'Jessica',
+    //             'last_name' => 'Alba'
+    //         ]
+    //     ];
+    //     return $users;
+    // }
 }
