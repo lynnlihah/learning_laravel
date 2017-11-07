f7863367f1fa1adf2c23e9a1b5128f98c286f7c0
lizhufeng <lzhf09@126.com>
2017-11-07 22:18:22 +0800

    learn user model&controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,6 +64,7 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            // 'name' => ucfirst($data['name']), // 创建用户时转换为大写
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
--- app/User.php
+++ app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    // setNameAttribute 方法是新增的 - 存入大些字符串，在model中实现
+    // 为什么是 setNameAttribute 呢？因为你是 name 属性，如果你的属性名叫 password, 可以这里就要变成 setPasswordAttribute。
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = ucfirst($value);
+    }
 }
